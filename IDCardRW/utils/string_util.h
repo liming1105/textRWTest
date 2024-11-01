51d0463aad9e9b28fea9de026c49ece4278f45b1
liming1105 <liming881105@gmail.com>
2024-11-01 21:54:34 +0800

    Update string_util.h

--- IDCardRW/utils/string_util.h
+++ IDCardRW/utils/string_util.h
@@ -7,7 +7,18 @@
 #include <iostream>
 #include <xlocbuf>
 #include <codecvt>
+
 using namespace std;
+
+#ifdef _UNICODE
+#define string_t wstring
+#else
+#define string_t string
+#endif
+
+//#define CODE_CONVERT_USE_CPP11
+#define CODE_CONVERT_USE_WIN32
+//#define CODE_CONVERT_USE_ICONV
 
 
 class string_util
